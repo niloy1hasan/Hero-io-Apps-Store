c178ef39f0b4285de19c934dc83a25e316ac0cf9
niloy1hasan <notecse23@gmail.com>
2025-10-08 20:46:34 +0600

    apps not found page added

---
+++ src/Components/AppsNotFound/AppsNotFound.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { useNavigate } from 'react-router';
+
+const AppsNotFound = () => {
+    const navigate = useNavigate();
+    const handleClick = () => {
+        navigate(-1);
+    }
+    return (
+        <section className='h-[100vh] w-10/12 mx-auto text-center flex justify-center items-center'>
+            <div>
+                <img src="/src/assets/app-error.png" className='w-[200px] h-[200px] lg:w-[300px] lg:h-[300px] mx-auto' alt="" />
+                <div className='my-8'>
+                    <h1 className='text-[#001931] font-bold text-3xl'>OPPS!! APP NOT FOUND</h1>
+                <p className='text-[#627382] py-2'>The App you are requesting is not found on our system.  please try another apps</p>
+                    <button onClick={handleClick} className='btn bg-gradient-to-r from-[#632EE3] to-[#9F62F2] text-white px-10 py-5 text-[16px] my-1'>Go Back!</button>
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default AppsNotFound;
