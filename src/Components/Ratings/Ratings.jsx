872ca2f18897348c92bddce50acb835715c3f5e3
niloy1hasan <notecse23@gmail.com>
2025-10-10 16:14:50 +0600

    rechart focus problem solved

--- src/Components/Ratings/Ratings.jsx
+++ src/Components/Ratings/Ratings.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import { Area, Bar, CartesianGrid, ComposedChart, Legend, Line, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts';
-
+import { Area, Bar, CartesianGrid, ComposedChart, Legend, Line, Rectangle, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts';
 
 
 const Ratings = ({ratings}) => {
@@ -33,15 +32,16 @@ console.log(ratings);
     return (
       <>
       <div className='w-[90%] mx-auto h-[1px] bg-gray-400'></div>
-      <div className='w-full h-[400px] my-10'>
+      <div className='w-full h-[300px] my-10'>
         <ResponsiveContainer>
-            <ComposedChart layout="vertical" data={ratings}
+            <ComposedChart margin={{ top: 5, right: 20, bottom: 5, left: 0 }} layout="vertical" data={ratings}
           >
             <CartesianGrid stroke="#f5f5f5" />
           <XAxis type="number" />
-          <YAxis dataKey="name" type="category" scale="band" />
+          <YAxis dataKey="name"  tick={{ fontSize: 12 } } width={50}
+                padding={{ top: 30, bottom: 30 }} type="category" scale="point" />
           <Tooltip />
-          <Bar dataKey="count" barSize={40} fill="#FF8811" />
+          <Bar dataKey="count" barSize={40} fill="#FF8811" activeBar={<Rectangle fill="#632EE3"/>} />
 
             </ComposedChart>
         </ResponsiveContainer>
